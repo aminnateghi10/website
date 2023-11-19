683c19047231a92c6ef4aec41216859c6aa587c9
aminnateghi10 <aminnateghi10@gmail.com>
2023-11-19 16:24:12 +0330

    fixt dark mode

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
+function changeToLight() {
+    $('.bg-dark').each(function () {
+        $(this).removeClass('bg-dark').addClass('bg-white');
+    });
+
+    $('.dark').each(function () {
+        $(this).addClass('light')
+        $(this).removeClass('dark')
+    });
+    $('.vertical-menu li .dark-mode span').text("حالت تاریک");
+    localStorage.setItem('dark-mode', 'light');
+};
 $('.change-dark-mode').click(() => {
 
     // Select all elements with class 'bg-white'
     let mode = localStorage.getItem('dark-mode');
 
-    if (mode === "light"){
-        localStorage.setItem('dark-mode','light');
+    if (mode === "light") {
         $('.bg-white').each(function () {
             $(this).removeClass('bg-white').addClass('bg-dark');
         });
@@ -13,19 +24,14 @@ $('.change-dark-mode').click(() => {
             $(this).addClass('dark')
             $(this).removeClass('light')
         });
-        localStorage.setItem('dark-mode','dark');
+        localStorage.setItem('dark-mode', 'dark');
         $('.vertical-menu li .dark-mode span').text("حالت روشن");
-    }else {
-        $('.bg-dark').each(function () {
-            $(this).removeClass('bg-dark').addClass('bg-white');
-        });
-
-        $('.dark').each(function () {
-            $(this).addClass('light')
-            $(this).removeClass('dark')
-        });
-        $('.vertical-menu li .dark-mode span').text("حالت تاریک");
-        localStorage.setItem('dark-mode','light');
+    } else {
+        changeToLight();
     }
 });
+let mode = localStorage.getItem('dark-mode');
 
+if (mode === "light") {
+    changeToLight();
+}
